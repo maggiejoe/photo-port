ab56c12672823ed23ef5fe04d0f084a4ae94164e
maggiejoe <maggiejoseph94@gmail.com>
2022-06-29 12:58:28 -0400

    committing for assistance with BCS on components displaying in browser

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section>
+            <h1 id="about">Who Am I?</h1>
+        </section>
+    );
+}
+
+export default About;
