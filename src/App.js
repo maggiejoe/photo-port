42a43de34a096bc33c7e7798944853d31bfbc69a
maggiejoe <maggiejoseph94@gmail.com>
2022-07-04 11:42:11 -0400

    contact form dynamically displays on click in navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ContactForm from './components/Contact';
 
 function App() {
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
       name: "commercial",
@@ -26,14 +28,21 @@ function App() {
   return (
     <div>
       <Nav
-      categories={categories}
-      setCurrentCategory={setCurrentCategory}
-      currentCategory={currentCategory}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-      <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div >
   );
